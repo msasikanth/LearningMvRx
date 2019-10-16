1e9788bb109e5bf87707686721e2827c6885cf0a
msasikanth <sasikanthmiriyampalli@gmail.com>
2019-10-16 19:35:34 +0530

    Add NavHostFragment to MainActivity
    - Update .gitignore

--- app/src/main/java/dev/sasikanth/mvrx/MainActivity.kt
+++ app/src/main/java/dev/sasikanth/mvrx/MainActivity.kt
@@ -1,12 +1,20 @@
 package dev.sasikanth.mvrx
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
     }
 }
